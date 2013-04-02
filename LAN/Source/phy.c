4bff322a59a05123ae543358c2b4f558a7cc037a
comparator <comparator@gmx.de>
2013-04-02 15:44:31 +0200

    Added Node Don't Work

--- LAN/Source/phy.c
+++ LAN/Source/phy.c
@@ -5,7 +5,10 @@
 #include "Phy\ENC28J60\enc28j60.h"
 #include "Phy\ENC28J60\net.h"
 
-uint8_t macaddr[6] = {0x00,0x04,0xA3,0x00,0x00,0x01}; // Microchip
+uint8_t macaddr[] = {0x00,0x04,0xA3,0x00,0x00,0x01}; // Microchip
+
+static uint8_t gwip[] = {0xFF,0xFF,0xFF,0xFF};
+static uint8_t gwmac[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
 
 // eth/ip buffer:
 static uint8_t buf[MAX_FRAMELEN];
@@ -49,32 +52,62 @@ MQ_t * LAN_GetBuf(void)
 {
   uint16_t plen;
   uint8_t type;
+  MQ_t * pTmp;
+  
   plen = enc28j60PacketReceive(MAX_FRAMELEN, buf);
 #ifdef  DHCP_client
   // DHCP renew IP:
   plen = packetloop_dhcp_renewhandler(buf, plen);
 #endif  //  DHCP_client
-  type = packetloop_arp_icmp(buf, plen);
-  if(type == 1)       // Packet for us
+  if((type = packetloop_arp_icmp(buf, plen)) != 0)    // 1 - Unicast, 2 - Broadcast
   {
+    if((buf[UDP_DST_PORT_H_P] == (MQTTS_UDP_PORT >> 8)) &&
+       (buf[UDP_DST_PORT_L_P] == (MQTTS_UDP_PORT & 0xFF)))
+    {
+      if(gwip[0] == 0xFF)
+      {
+        if(buf[UDP_DATA_P + 1] == MQTTS_MSGTYP_GWINFO)
+        {
+          memcpy(gwmac, &buf[ETH_SRC_MAC], 6);
+          memcpy(gwip,  &buf[IP_SRC_P], 4);
+        }
+        else
+          return NULL;
+      }
+      
+      plen -= UDP_DATA_P;
+      if((plen > sizeof(MQTTS_MESSAGE_t)) || ((pTmp = mqAssert()) == NULL))
+        return NULL;
+      memcpy(&pTmp->mq, &buf[UDP_DATA_P], plen);
+      return pTmp;
+    }
   }
-  else if(type == 2)  // Broadcast
-  {
-  }
-
-
-/*
-  
-  type = 
-
-
-  {
-   uint8_t * pTmp;
-                    pTmp = (uint8_t *)mqAssert();
-  }
-*/
 
   return NULL;
+}
+
+void LAN_Send(MQ_t * pBuf)
+{
+  if(pBuf->mq.MsgType == MQTTS_MSGTYP_SEARCHGW)
+  {
+    uint8_t i = 0;
+    while(i < 4)
+    {
+      gwip[i] = 0xFF;
+      gwmac[i] = 0xFF;
+      i++;
+    }
+    gwmac[4] = 0xFF;
+    gwmac[5] = 0xFF;
+  }
+  send_udp_prepare(buf, MQTTS_UDP_PORT, gwip, MQTTS_UDP_PORT, gwmac);
+  
+  uint16_t datalen = pBuf->mq.Length;
+
+  memcpy(&buf[UDP_DATA_P], &pBuf->mq, datalen);
+  mqRelease(pBuf);
+  
+  send_udp_transmit(buf, datalen);
 }
 
 #endif  //  (defined ENC28J60)
---
+++ LAN/Source/util.h
@@ -0,0 +1,18 @@
+/*
+Copyright (c) 2011-2013 <comparator@gmx.de>
+
+This file is part of the X13.Home project.
+http://X13home.github.com
+
+BSD New License
+See LICENSE.txt file for license details.
+*/
+
+#ifndef _UTIL_H
+#define _UTIL_H
+
+//uint8_t sprintstrP(uint8_t *pBuf, const char *s);
+uint8_t sprinthex(uint8_t *pBuf, uint8_t val);
+uint8_t sprintdec(uint8_t *pBuf, uint16_t val);
+
+#endif
