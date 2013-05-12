96f8831023af8c429c466068fcb340bd6e264fd5
comparator <comparator@gmx.de>
2013-05-12 18:12:22 +0200

    Gateway Test Version

--- Source/RF_Gate.c
+++ Source/RF_Gate.c
@@ -8,7 +8,7 @@ BSD New License
 See LICENSE.txt file for license details.
 */
 
-#error "Sorry, but its doesn't work, wait a little bit."
+//#error "Sorry, but its doesn't work, wait a little bit."
 
 #include "config.h"
 
@@ -20,82 +20,101 @@ int main(void) __attribute__((naked));
 int main(void)
 {
 // Watchdog Stop
-    cli();
-    wdt_reset();
-    wdt_disable();
-    // Configure Power Reduction
-    CONFIG_PRR();
-    // Initialize Memory manager
-    mqInit();
-    // Initialize Object's Dictionary
-    InitOD();
-    // Init MQTTS
-    MQTTS_Init();
-    // Init Interconnection Interface & Load Configuration
-    PHY_Init();
-    InitUART(USART_BAUD);   //  Buad = 38400, fosc = 16M/ (16 * baud)  - 1
+  cli();
+  wdt_reset();
+  wdt_disable();
+  // Configure Power Reduction
+  CONFIG_PRR();
+  // Initialize Memory manager
+  mqInit();
+  // Initialize Object's Dictionary
+  InitOD();
+  // Init MQTTS
+  MQTTS_Init();
+  // Init Interconnection Interface & Load Configuration
+  PHY_Init();
+  // Init UART
+  InitUART(USART_BAUD);   //  Buad = 38400, fosc = 16M/ (16 * baud)  - 1
+  // Initialise  variables
+  iPool = 0;
 
-    // Initialise  variables
-    iPool = 0;
-    
-    //MQ_t * pUBuf = NULL;        // USART Buffer
-    MQ_t * pRBuf = NULL;        // RF Buffer
-    MQ_t * pMBuf = NULL;        // MQTTS Buffer
-    uint8_t * pPBuf = NULL;     // Publish Buffer
-    
-    uint8_t bTmp;
-    uint16_t poolIdx = 0xFFFF;
+  MQ_t * pUBuf = NULL;        // USART Buffer
+  MQ_t * pRBuf = NULL;        // RF Buffer
+  MQ_t * pMBuf = NULL;        // MQTTS Buffer
+  uint8_t * pPBuf = NULL;     // Publish Buffer
 
-    s_Addr sAddr;
+  uint8_t objLen;
+  uint16_t poolIdx = 0xFFFF;
 
-    // Initialize Task Planer
-    InitTimer();
-    // configure Sleep controller & enable interrupts
-    set_sleep_mode(SLEEP_MODE_IDLE);    // Standby, Idle
-    sei();                              // Enable global interrupts
+  s_Addr sAddr;
 
-    // 
-    PHY_Start();
+  // Initialize Task Planer
+  InitTimer();
+  // configure Sleep controller & enable interrupts
+  set_sleep_mode(SLEEP_MODE_IDLE);    // Standby, Idle
+  sei();                              // Enable global interrupts
+  // 
+  PHY_Start();
 
-    while(1)
+  while(1)
+  {
+    pUBuf = (MQ_t *)uGetBuf(&sAddr);
+    if(pUBuf != NULL)
     {
-      pRBuf = PHY_GetBuf(&sAddr);
-      if((pRBuf != NULL) && (MQTTS_Parser(pRBuf, &sAddr) == 0))
-        mqRelease(pRBuf);
-        
-      pMBuf = MQTTS_Get(&sAddr);
-      if(pMBuf != NULL)
-        PHY_Send(pMBuf, &sAddr);
-        
-      if(iPool & IPOOL_USR)
+      if((sAddr == rf_GetNodeID()) || (sAddr == 0))
       {
-        iPool &= ~IPOOL_USR;
-
-        bTmp = MQTTS_GetStatus();
-        if(bTmp == MQTTS_STATUS_CONNECT)
+        if(MQTTS_Parser(pUBuf,&sAddr) == 0)
         {
-          if(poolIdx == 0xFFFF)
-            poolIdx = PoolOD();
-            
-          if(poolIdx != 0xFFFF)
+          if((MQTTS_GetStatus() == MQTTS_STATUS_CONNECT) && (sAddr == 0))  // broadcast
+            PHY_Send(pUBuf, &sAddr);
+          else
+            mqRelease(pUBuf);
+        }
+      }
+      else if(MQTTS_GetStatus() == MQTTS_STATUS_CONNECT)
+        PHY_Send(pUBuf, &sAddr);
+    }
+
+    pRBuf = PHY_GetBuf(&sAddr);
+    if(pRBuf != NULL)
+    {
+      if(MQTTS_GetStatus() == MQTTS_STATUS_CONNECT)
+        uPutBuf((uint8_t *)pRBuf, &sAddr);
+      else
+        mqRelease(pRBuf);
+    }
+
+    pMBuf = MQTTS_Get(&sAddr);
+    if(pMBuf != NULL)
+      uPutBuf((uint8_t *)pMBuf, &sAddr);
+
+    if(iPool & IPOOL_USR)
+    {
+      iPool &= ~IPOOL_USR;
+
+      if(MQTTS_GetStatus() == MQTTS_STATUS_CONNECT)
+      {
+        if(poolIdx == 0xFFFF)
+          poolIdx = PoolOD();
+
+        if(poolIdx != 0xFFFF)
+        {
+          // Publish
+          pPBuf = (uint8_t *)mqAssert();
+          if(pPBuf != NULL)
           {
-            // Publish
-            pPBuf = (uint8_t *)mqAssert();
-            if(pPBuf != NULL)
-            {
-              bTmp = (MQTTS_MSG_SIZE - MQTTS_SIZEOF_MSG_PUBLISH);
-              
-              ReadOD(poolIdx, MQTTS_FL_TOPICID_NORM | 0x80, &bTmp, pPBuf);
-              MQTTS_Publish(poolIdx, MQTTS_FL_QOS1, bTmp, pPBuf);
-              mqRelease((MQ_t *)pPBuf);
-              poolIdx = 0xFFFF;
-            }
+            objLen = (MQTTS_MSG_SIZE - MQTTS_SIZEOF_MSG_PUBLISH);
+
+            ReadOD(poolIdx, MQTTS_FL_TOPICID_NORM | 0x80, &objLen, pPBuf);
+            MQTTS_Publish(poolIdx, MQTTS_FL_QOS1, objLen, pPBuf);
+            mqRelease((MQ_t *)pPBuf);
+            poolIdx = 0xFFFF;
           }
         }
-
-        bTmp = MQTTS_Pool(poolIdx != 0xFFFF);
       }
+      MQTTS_Pool(poolIdx != 0xFFFF);
     }
+  }
 }
 
 ISR(TIMER_ISR)
@@ -119,15 +138,13 @@ ISR(TIMER_ISR)
 
     iPool &= ~IPOOL_CALIB;
   }
-  else if(!(iPool & IPOOL_SLEEP))
+  else
   {
     TCNT1 = 0;
     TCCR1B = (2<<CS10);
     iPool |= IPOOL_CALIB;
   }
 #endif  //  USE_RTC_OSC
-  {
-    iPool |= IPOOL_USR;
-    PHY_Pool();
-  }
+  iPool |= IPOOL_USR;
+  PHY_Pool();
 }
