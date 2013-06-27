8290e8b735bedcfab77b09c44ce2b9d930c87bd9
comparator <comparator@gmx.de>
2013-06-27 22:00:20 +0200

    BlinkM driver work with bus planer.

--- MQTTS/Source/ObjDict.h
+++ MQTTS/Source/ObjDict.h
@@ -57,6 +57,8 @@ enum
     objUInt16   = 'W',  // uint16
     objInt32    = 'd',  // int32
     objUInt32   = 'D',  // uint32
+    objInt64    = 'q',  // int64
+    objUInt64   = 'Q',  // uint64
     objString   = 's',  // UTF8 '0' - terminated string
     objArray    = 'a',  // array
     objUnknown  = 'U',  // unknown
